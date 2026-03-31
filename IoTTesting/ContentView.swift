1312bacbbc5b478dcaee8126ba6f3f5580161eeb
Patrick Sinaga <sinagapatrick610@gmail.com>
2026-03-31 20:24:41 +0700

    Initial Commit

---
+++ IoTTesting/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IoTTesting
+//
+//  Created by Patrick Samuel Owen Saritua Sinaga on 31/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IoTTesting/IoTTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  IoTTestingApp.swift
+//  IoTTesting
+//
+//  Created by Patrick Samuel Owen Saritua Sinaga on 31/03/26.
+//
+
+import SwiftUI
+
+@main
+struct IoTTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
